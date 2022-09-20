0b5dfe9158d048d5e9018bdd35db64113105ddfc
ERIC1CN <111236481+ERIC1CN@users.noreply.github.com>
2022-09-21 00:28:51 +0800

    Add files via upload

---
+++ 第一周_程序设计与C语言/#1--Hello World!.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	return 0;
+	
+}
+// 将 int 改为 void ,就不需要写“return 0;” 
+
---
+++ 第一周_程序设计与C语言/#2--计算器.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("现在是%d年\n",2000+22);
+	return 0;
+}
